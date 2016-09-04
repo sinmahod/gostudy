17d24e742df1819440b2def756d8deb2ccd15cdb
sinmahod <sinmahod@qq.com>
2016-09-05 00:28:15 +0800

    update

--- test.go
+++ test.go
@@ -32,6 +32,11 @@ const (
 	I         //6
 )
 
+//定义函数（自定义类型）
+type Circle2 struct {
+	rad int
+}
+
 func main() {
 	fmt.Println("Hello World!", a, b, c)
 
@@ -96,4 +101,120 @@ func main() {
 	fmt.Println("得到变量的指针：", *prt)
 	area = 33 //变量area的值改变了那么指针的值就会改变
 	fmt.Println("得到变量的指针：", *prt)
+
+	//尝试条件语句if else
+	if len(A) > 5 {
+		fmt.Println(len(A))
+	} else {
+		fmt.Println(2)
+	}
+
+	//尝试条件语句switch
+	switch i {
+	case 1, 2, 31:
+		fmt.Println(1)
+		break
+	case 3, 4, 5:
+		fmt.Println(2)
+		break
+	default:
+		fmt.Println(3)
+	}
+
+	//尝试条件语句select
+	var c1, c2, c3 chan int
+	var i1, i2 int
+
+	select {
+	case i1 = <-c1: //这个不懂，如何使这个成立呢？
+		fmt.Printf("received ", i1, " from c1\n")
+	case c2 <- i2:
+		fmt.Printf("sent ", i2, " to c2\n")
+	case i3, ok := (<-c3): // same as: i3, ok := <-c3
+		if ok {
+			fmt.Printf("received ", i3, " from c3\n")
+		} else {
+			fmt.Printf("c3 is closed\n")
+		}
+	default:
+		fmt.Printf("no communication\n")
+	}
+
+	//for循环使用1 类似java for i=2
+	for x := 0; x < i; x++ {
+		fmt.Println("for循环普通用法", x)
+	}
+	//for循环使用2 类似java while
+	x := 0
+	for x < i {
+		fmt.Println("for循环while用法", x)
+		x++
+	}
+	//for循环使用3 for each  map用法
+	var mp map[string]string = make(map[string]string)
+	mp["key1"] = "v1"
+	mp["key2"] = "v2"
+	//记住关键字range
+	for key := range mp {
+		fmt.Println(key, mp[key])
+	}
+	//for循环 无限循环
+	for {
+		if i > 3 {
+			break //还有goto 、continue
+		}
+		fmt.Println("for循环无限循环", i)
+		i++
+	}
+
+	//调用测试环境
+	fmt.Println("调用函数测试", test(1))
+	fmt.Println("调用函数测试", test(10))
+	x1, x2 := test2(3, 5)
+	x3, x4 := test2(5, 3)
+	fmt.Println("调用双返回函数测试", x1, x2)
+	fmt.Println("调用双返回函数测试", x3, x4)
+
+	//测试匿名函数，如果不进行声明的话只能得到函数的内存地址
+	functest := test3()
+	fmt.Println("调用函数闭包，匿名函数", functest())
+	fmt.Println("调用函数闭包，匿名函数", functest())
+	fmt.Println("调用函数闭包，匿名函数", functest())
+
+	//调用方法
+	var cs Circle2
+	cs.rad = 100
+	fmt.Println("调用方法", cs.getC())
+}
+
+//创建函数 接受Int参数返回string
+func test(i int) bool {
+	if i < 5 {
+		return true
+	} else {
+		return false
+	}
+}
+
+//创建双返回函数 接受Int参数返回string和int
+func test2(i, j int) (bool, int) {
+	if i < 5 {
+		return true, i + j
+	} else {
+		return false, i - j
+	}
+}
+
+//创建函数闭包，测试匿名函数，
+func test3() func() int {
+	i := 0
+	return func() int {
+		i++
+		return i
+	}
+}
+
+//创建方法
+func (c Circle2) getC() int {
+	return c.rad + 10
 }
